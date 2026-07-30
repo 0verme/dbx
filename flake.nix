beaf52582f94194bdaf37bd3941ed597a21d2ff7
t8y2 <1156263951@qq.com>
2026-07-30 09:43:16 +0800

    chore(release): prepare v0.5.70

--- flake.nix
+++ flake.nix
@@ -184,7 +184,7 @@
         # ------------------------------------------------------------------ #
         packages.dbx-desktop = pkgs.stdenv.mkDerivation (finalAttrs: {
           pname = "dbx-desktop";
-          version = "0.5.69";
+          version = "0.5.70";
 
           src = pkgs.lib.cleanSource ./.;
 
