8501b10ea7be7d6955139a91d1b07aa5787df36a
t8y2 <1156263951@qq.com>
2026-08-10 12:17:23 +0800

    fix(mqtt): format TLS regression test
    Refs: #5722

--- apps/desktop/src/lib/__tests__/connection/mqttConnectionDialog.spec.ts
+++ apps/desktop/src/lib/__tests__/connection/mqttConnectionDialog.spec.ts
@@ -2,8 +2,8 @@ import { readFileSync } from "node:fs";
 import { describe, expect, it } from "vitest";
 
 const dialogSource = readFileSync(new URL("../../../components/connection/ConnectionDialog.vue", import.meta.url), "utf8");
-const mqttSectionStart = dialogSource.indexOf('<template v-else-if="form.db_type === \'mqtt\'">');
-const mqttSectionEnd = dialogSource.indexOf('<template v-else-if="form.db_type === \'victoriametrics\'">', mqttSectionStart);
+const mqttSectionStart = dialogSource.indexOf("<template v-else-if=\"form.db_type === 'mqtt'\">");
+const mqttSectionEnd = dialogSource.indexOf("<template v-else-if=\"form.db_type === 'victoriametrics'\">", mqttSectionStart);
 const mqttSection = dialogSource.slice(mqttSectionStart, mqttSectionEnd);
 
 describe("MQTT connection dialog", () => {
